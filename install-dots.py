2e9bf0b2037aac97545033715f0a0d5375ce7c46
Lorenyx <l0r3nyx@gmail.com>
2022-10-07 14:26:39 -0500

    Added pathlib

--- install-dots.py
+++ install-dots.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 import shutil, os
 
+from pathlib import Path
 from requests import get
 
 if __name__ == '__main__':
     print('[=] Installing dot files...')
     # --[ Move files to diretories ]--
     #TODO Ask before overwriting
-    shutil.copy(r'./dots/.zshrc', r'~/.zshrc')
+    shutil.copy(r'./dots/.zshrc', Path('~/.zshrc'))
     os.chmod('~/.zshrc', 0o755) # rwx|r-x|r-x
-    shutil.copy(r'./dots/.pwninit-template.py', r'~/.config/pwninit-template.py')
-    shutil.copy(r'./dots/terminator.config', r'~/.config/terminator/config')
+    shutil.copy(r'./dots/.pwninit-template.py', Path('~/.config/pwninit-template.py'))
+    shutil.copy(r'./dots/terminator.config', Path('~/.config/terminator/config'))
     # --[ Install cutter ]--
     print('[=] Downloading "cutter"...')
     url = 'https://github.com/rizinorg/cutter/releases/download/v2.1.2/Cutter-v2.1.2-Linux-x86_64.AppImage'
     r = get(url)
     with open('./downloads/cutter', 'wb') as f:
         f.write(r.content)
-    shutil.move('./downloads/cutter', '~/.local/bin/cutter')
+    shutil.move('./downloads/cutter', Path('~/.local/bin/cutter'))
     os.chmod('~/.local/bin/cutter', 0o755) # rwx|r-x|r-x
     print('[+] Done')
